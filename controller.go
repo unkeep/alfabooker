7910017fe83c814a7a19657484003e22613a6f1a
aliaksei.ankip <aliaksei.ankip@solarwinds.com>
2019-04-02 09:57:13 +0300

    chaching auth token in file

--- controller.go
+++ controller.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"golang.org/x/net/context"
@@ -17,6 +19,7 @@ type Controller struct {
 	account           Account
 	telegram          Telegram
 	pendingOperations map[string]Operation
+	googleAuthCode    string
 }
 
 const IgnoreOptionID = "IgnoreOptionID"
@@ -121,8 +124,35 @@ func (c *Controller) handleOperationReply(opReply OperationReply) {
 
 // Retrieve a token, saves the token, then returns the generated client.
 func (c *Controller) getClient(config *oauth2.Config) *http.Client {
-	tok := c.getTokenFromWeb(config)
+	tokFile := "token.json"
+	tok, err := c.tokenFromFile(tokFile)
+	if err != nil {
+		tok = c.getTokenFromWeb(config)
+		c.saveToken(tokFile, tok)
+	}
 	return config.Client(context.Background(), tok)
+}
+
+func (c *Controller) tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func (c *Controller) saveToken(path string, token *oauth2.Token) {
+	log.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 // Request a token from the web, then returns the retrieved token.
